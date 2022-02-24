c8ec2e6a57791e36d5868360a8b171d7bfa31472
Brian Thomas <btdeveloper125@gmail.com>
2022-02-24 16:34:03 -0700

    refactor(Home): Add container

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,22 @@
 import React from "react"
 import { Helmet } from 'react-helmet'
+// components
+import Layout from "../components/Layout"
+// styles
 import styled from "@emotion/styled"
 import { css } from "@emotion/react"
-
-const Container = styled.div`
-  margin:3em auto;
-  max-width:990px;
-  background-color: red;
+const Container = styled.section`
+  max-width:1020px;
+  margin:0 auto;
 `
 
 export default function Home() {
   return(
-    <Container>
+    <Layout>
       <Helmet title="Home"/>
-      <p>hello</p>
-    </Container>
+      <Container>
+        <p>hello</p>
+      </Container>
+    </Layout>
   )
 }
