bd0f7d9acbbf54f3ac434390d569958d123b0ad2
Brian Thomas <btdeveloper125@gmail.com>
2022-02-24 10:55:05 -0700

    feat(Setup`): Main setup for react helmet, manifest, service worker

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,25 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon:`src/images/icon.png`
+      },
+    },
+    {
+      resolve: `gatsby-plugin-offline`,
+      options: {
+        precachePages: [`/index/*`],
+      },
+    },
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,11 @@
 import React from "react"
+import { Helmet } from 'react-helmet'
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return(
+    <>
+      <Helmet title="Home"/>
+      <p>hello</p>
+    </>
+  )
 }
