d5a57f95c73e8e54df0dfa9c3658ef94e4cf99c1
Brian Thomas <btdeveloper125@gmail.com>
2022-02-25 11:17:08 -0700

    feat(Spinner) Add spinner component

---
+++ src/components/Spinner.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { css, jsx } from '@emotion/react'
+import { usePromiseTracker } from "react-promise-tracker";
+import ClipLoader from 'react-spinners/ClipLoader'
+
+export const LoadingSpinnerComponent = (props) => {
+const { promiseInProgress } = usePromiseTracker();
+const override = css`
+  display: block;
+  margin: 0 auto;
+  border-color: red;
+`;
+const color = css`color: var(--main-color-light);`
+  return (
+    <div
+      css={css`
+          margin:0 auto;
+          min-height: 60px;
+          `}
+    >
+    {
+      (promiseInProgress === true) ?
+      <ClipLoader color={color} css={override} size={50} />
+      :
+        null
+    }
+  </div>
+  )
+};
