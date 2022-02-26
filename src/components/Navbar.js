95e3d7dadb9f933cc0626716ae59d2afe99418eb
Brian Thomas <btdeveloper125@gmail.com>
2022-02-26 12:49:36 -0700

    feat(Navbar) Add logout button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,17 +4,29 @@ import { Toaster } from "react-hot-toast"
 
 const NavbarContainer = styled.nav`
   display:flex;
-  justify-content: center;
+  justify-content: space-around;
   align-items:center;
   height:15vh;
   font-size: 2.5rem;
   margin-bottom: 3rem;
 `
 function Navbar() {
+  const logout = () => {
+    localStorage.removeItem('token')
+    localStorage.removeItem('user')
+    localStorage.removeItem('jobs')
+    return window.location.href = '/login'
+  }
+  useEffect(() =>{
+    if(!localStorage.getItem('token')){
+      // return window.location.href = '/login'
+    }
+  })
   return (
     <NavbarContainer>
       <Toaster />
       <p>Jobz</p>
+      <p onClick={() => logout()}>logout</p>
     </NavbarContainer>
   )
 }
