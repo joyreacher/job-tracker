35d90c1b50a6897355ea039526af7d6d78392263
Brian Thomas <btdeveloper125@gmail.com>
2022-02-26 12:52:32 -0700

    refactor(AllJobs) Setup to style each job entry

--- src/components/jobs/AllJobs.js
+++ src/components/jobs/AllJobs.js
@@ -1,5 +1,15 @@
 import React, { useEffect } from "react"
+// styles
+import styled from "@emotion/styled"
+import { jsx, css } from "@emotion/react"
+const breakpoints = [376, 411, 576, 768, 1020, 1200]
+const mq = breakpoints.map(
+  bp => `@media (max-width: ${bp}px)`
+)
 
+const Container = styled.section`
+  position:relative;
+`
 function AllJobs({jobs}) {
   /**
    * @function storeJobs
@@ -22,17 +32,25 @@ function AllJobs({jobs}) {
      */
     const result = JSON.parse(localStorage.getItem('jobs'))
     return(
-      <>
-      <p>this is the alll jobs ccomponent</p>
-      <p>This is from localStorage</p>
+      <Container>
+      <h1>this is the alll jobs ccomponent</h1>
+      <h2>This is from localStorage</h2>
       {
         result.map(element => {
           return(
-              <p key={element._id}>{element.role}</p>
+            <div key={element._id}>
+              <h3>{element.company}</h3>
+              <p>{element.role}</p>
+              <p>{element.contact}</p>
+              <p>{element.location}</p>
+              <p>{element.source}</p>
+              <p>{element.link}</p>
+              <p>{element.notes}</p>
+          </div>
             )
         })
       }
-    </>
+    </Container>
     )
   }
   /** This will run when localstorage jobs value is not defined */
@@ -42,9 +60,16 @@ function AllJobs({jobs}) {
       <p>This is from props</p>
       {
         jobs.map(element => {
-          console.log(element.role)
           return(
-              <p key={element._id}>{element.role}</p>
+            <div key={element._id}>
+              <h3>{element.company}</h3>
+              <p>{element.role}</p>
+              <p>{element.contact}</p>
+              <p>{element.location}</p>
+              <p>{element.source}</p>
+              <p>{element.link}</p>
+              <p>{element.notes}</p>
+          </div>
             )
         })
       }
