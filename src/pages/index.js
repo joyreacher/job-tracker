5c6a0e4ccb6ae4a90aa1de7e54490e07f70ffeb0
Brian Thomas <btdeveloper125@gmail.com>
2022-02-26 10:11:14 -0700

    refactor(Main Page): How a user will be routed back to login page
    checkToken() checks localStorage for token
    
    It is then set with the GET request to retrieve all jobs entered into the database
    All jobs entered by every user is returned

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,6 +11,41 @@ const Container = styled.section`
 `
 
 export default function Home() {
+  const checkForToken = () => {
+    const token = localStorage.getItem('token')
+    return token
+  }
+  useEffect(() =>{
+    
+    if(!checkForToken() || !checkForToken() === undefined){
+      return window.location.href = '/login'
+    }else{
+      const token = checkForToken()
+      trackPromise(
+        axios({
+          url: 'https://jobz-api.herokuapp.com/jobs',
+          method: 'GET',
+          headers: {
+            'Accept': 'application/json',
+            'Content-type': 'application/json;charset=UTF-8',
+            "Authorization" : `Bearer ${token}`
+          },
+          // data: {
+          //   Username: e.target[0].value,
+          //   Password: e.target[1].value
+          // }
+        })
+        .then(response => {
+          setJobs(JSON.stringify(response.data))
+          console.log(jobs)
+        })
+        .catch((error) => {
+          return toast.error('whoa something happened')
+        })
+      )
+    }
+    
+  },[])
   return(
     <Layout>
       <Helmet title="Home"/>
