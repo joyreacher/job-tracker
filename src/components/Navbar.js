08f16340f3b62d82bbd459556331521452bdf93b
Brian Thomas <btdeveloper125@gmail.com>
2022-03-04 18:09:55 -0700

    refactor: Navbar container styles/format

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,12 +3,40 @@ import styled from "@emotion/styled"
 import { Toaster } from "react-hot-toast"
 
 const NavbarContainer = styled.nav`
-  display:flex;
-  justify-content: space-around;
   align-items:center;
-  height:15vh;
-  font-size: 2.5rem;
+  height:20vh;
+  font-size: 1.5rem;
   margin-bottom: 3rem;
+  background-color: var(--color-navbar);
+`
+const InnerContainer = styled.div`
+  height:100%;
+  align-items: center;
+  margin:0 auto;
+  display:flex;
+  justify-content: space-between;
+  max-width: 1020px;
+`
+const MenuContainer = styled.div`
+  font-size: 1.1rem;
+  display:flex;
+  align-items:center;
+  width:25vw;
+  justify-content: space-between;
+  
+`
+const Menu = styled.div`
+  display:flex;
+  justify-content:center;
+  text-align:center;
+  font-size: 1rem;
+  background-color: var(--color-main-dark);
+  height:60px;
+  width:60px;
+  border-radius: 45px;
+`
+const MenuText = styled.p`
+  align-self: center;
 `
 const LogOutLink = styled.p`
   cursor: pointer;
@@ -27,9 +55,18 @@ function Navbar() {
   })
   return (
     <NavbarContainer>
-      <Toaster />
-      <p>Jobz</p>
-      <LogOutLink onClick={() => logout()}>logout</LogOutLink>
+      <InnerContainer>
+        <Toaster />
+        <p>Jobby</p>
+        <MenuContainer>
+          <LogOutLink onClick={() => logout()}>logout</LogOutLink>
+          <Menu>
+            <MenuText>
+              Add Job
+            </MenuText>
+          </Menu>
+        </MenuContainer>
+      </InnerContainer>
     </NavbarContainer>
   )
 }
