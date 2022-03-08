929cc26cfa69540fe13faa002387216734d82aaa
Brian Thomas <btdeveloper125@gmail.com>
2022-03-08 11:52:26 -0700

    feat: Add link to Serenity font

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -9,6 +9,7 @@ const Wrapper = styled("main")`
     background-color: var(--color-main-dark);
     min-height: 100vh;
     font-size: clamp(1.2rem, .5vw, 1.5rem);
+    font-family: serenity, sans-serif;
 `
 
 export default function Layout({ children }) {
