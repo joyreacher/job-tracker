27c8d9228606683554d15e941516bd598a8cc967
Brian Thomas <btdeveloper125@gmail.com>
2022-03-22 10:51:38 -0600

    feat: Iniitalize icon box with filter timeline

--- src/pages/index.js
+++ src/pages/index.js
@@ -83,7 +83,15 @@ export default function Home() {
       .then(() => {
         setIsloading(false)
       })
+      .then(() =>{
+        fliterTl.to('#icon-box', {
+          ease:'none',
+          duration:.25,
+          boxShadow: `inset -2px -2px 5px rgba(255, 255, 255, 1),
+          inset 3px 3px 5px rgba(0, 0, 0, 0.1)`
+        })
         flipInit()
+      })
     )
   }
 
