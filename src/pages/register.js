3f1cba6c701ddc07c70437e6bfe38fdde8397fd2
Brian Thomas <btdeveloper125@gmail.com>
2022-04-08 13:36:15 -0600

    refactor(Register Page): Update to include the logged out navbar and the seo component

--- src/pages/register.js
+++ src/pages/register.js
@@ -2,6 +2,8 @@ import React, { useEffect } from "react"
 import { Helmet } from 'react-helmet'
 // components
 import Layout from "../components/Layout"
+import Seo from '../components/Seo'
+import Navbar from "../components/Navbar"
 // styles
 import styled from "@emotion/styled"
 import { css } from "@emotion/react"
@@ -11,11 +13,20 @@ const Section = styled.section`
   height:100%;
   max-width:1020px;
 `
+const Headline = styled.h1`
+  text-align:center;
+`
 function Register() {
   return (
     <Layout>
-      <Helmet title="Register" />
+    <Seo
+          title="Register"
+          description="Register to use Jobby"
+          lang="en"
+        />
+      <Navbar />
       <Section>
+        <Headline>Register</Headline>
         <RegisterForm />
       </Section>
     </Layout>
